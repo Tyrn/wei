194df6127e4f0bfa8463f3191488bd62815b845f
Tyrn <alexey1911@gmail.com>
2025-02-19 23:41:32 +0300

    typst-compile-all.sh script added

---
+++ typst-compile-all.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# set -e
+set -u
+set -x
+
+typst compile the-hundred-squares-ru.typ        the-hundred-squares-ru.pdf
+typst compile case-of-the-azure-letter-ru.typ   case-of-the-azure-letter-ru.pdf
+typst compile a-case-of-the-missing-god-ru.typ  a-case-of-the-missing-god-ru.pdf
+typst compile wizards-and-ministers-ru.typ      wizards-and-ministers-ru.pdf
+typst compile the-insider-ru.typ                the-insider-ru.pdf
