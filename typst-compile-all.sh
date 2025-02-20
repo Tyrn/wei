d244bdcbe0031645b308af2b1bf8a8476ff6036d
Tyrn <alexey1911@gmail.com>
2025-02-20 08:35:01 +0300

    the-story-of-queen-cassia-ru.typ to typst-compile-all.sh

--- typst-compile-all.sh
+++ typst-compile-all.sh
@@ -4,6 +4,7 @@ set -u
 set -x
 
 typst compile the-hundred-squares-ru.typ        the-hundred-squares-ru.pdf
+typst compile the-story-of-queen-cassia-ru.typ  the-story-of-queen-cassia-ru.pdf
 typst compile case-of-the-azure-letter-ru.typ   case-of-the-azure-letter-ru.pdf
 typst compile a-case-of-the-missing-god-ru.typ  a-case-of-the-missing-god-ru.pdf
 typst compile wizards-and-ministers-ru.typ      wizards-and-ministers-ru.pdf
