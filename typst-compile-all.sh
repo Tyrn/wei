7636258319a0421a93b8a9eb4818aeb6f5328e04
Tyrn <alexey1911@gmail.com>
2025-02-20 10:48:32 +0300

    the-golden-king, full house now! typst 0.12

--- typst-compile-all.sh
+++ typst-compile-all.sh
@@ -2,10 +2,10 @@
 # set -e
 set -u
 set -x
-
-typst compile the-hundred-squares-ru.typ        the-hundred-squares-ru.pdf
-typst compile the-story-of-queen-casia-ru.typ   the-story-of-queen-casia-ru.pdf
-typst compile case-of-the-azure-letter-ru.typ   case-of-the-azure-letter-ru.pdf
-typst compile a-case-of-the-missing-god-ru.typ  a-case-of-the-missing-god-ru.pdf
-typst compile wizards-and-ministers-ru.typ      wizards-and-ministers-ru.pdf
-typst compile the-insider-ru.typ                the-insider-ru.pdf
+typst compile the-story-of-the-golden-king-ru.typ  the-story-of-the-golden-king-ru.pdf
+typst compile the-hundred-squares-ru.typ           the-hundred-squares-ru.pdf
+typst compile the-story-of-queen-casia-ru.typ      the-story-of-queen-casia-ru.pdf
+typst compile case-of-the-azure-letter-ru.typ      case-of-the-azure-letter-ru.pdf
+typst compile a-case-of-the-missing-god-ru.typ     a-case-of-the-missing-god-ru.pdf
+typst compile wizards-and-ministers-ru.typ         wizards-and-ministers-ru.pdf
+typst compile the-insider-ru.typ                   the-insider-ru.pdf
