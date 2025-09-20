9762d6eb7b80c38807af5ce9f5a275a1d0560edf
Tyrn <alexey1911@gmail.com>
2025-09-20 17:35:53 +0300

    build_dir to typst-compile-all.sh

--- typst-compile-all.sh
+++ typst-compile-all.sh
@@ -5,7 +5,16 @@ set -u
 
 # ✔️ ✅
 
+build_dir="wei-pdf"
+
+if [ ! $# -eq 0 ]; then
+  build_dir="${1}"
+fi
+
+mkdir -p "$build_dir"
+
+printf "To \"%s\":\n" "$build_dir"
 for f in 0*.typ; do
   printf "%-45s" "$f..."
-  typst compile "$f" "${f%.typ}.pdf" && printf "✔️  %s.pdf\n" "${f%.typ}" || echo "❌ Failed: $f"
+  typst compile "$f" "$build_dir/${f%.typ}.pdf" && printf "✔️  %s.pdf\n" "${f%.typ}" || echo "❌ Failed: $f"
 done
